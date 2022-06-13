94e34bb06956fd2320f58592cca26e095ccd8f40
ryanwalker <ryanwalker55@gmail.com>
2022-06-13 09:51:02 -0700

    Create PrettyPrint.java
    Class to pretty print XML onto the test case view.

---
+++ src/org/walker/tprDBHelper/workers/PrettyPrint.java
@@ -0,0 +1,96 @@
+package com.journaldev.java.xmlutil;
+
+import org.apache.xml.serialize.OutputFormat;
+import org.apache.xml.serialize.XMLSerializer;
+import org.w3c.dom.Document;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
+import java.io.IOException;
+import java.io.StringReader;
+import java.io.StringWriter;
+import java.io.Writer;
+
+public class PrettyPrint {
+
+    /**
+     *
+     * @param unformattedXml - XML String
+     * @return Properly formatted XML String
+     */
+    public static String format(String unformattedXml) {
+        try {
+            Document document = parseXmlFile(unformattedXml);
+
+            OutputFormat format = new OutputFormat(document);
+            format.setLineWidth(65);
+            format.setIndenting(true);
+            format.setIndent(2);
+            Writer out = new StringWriter();
+            XMLSerializer serializer = new XMLSerializer(out, format);
+            serializer.serialize(document);
+
+            return out.toString();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return "";
+        }
+
+    }
+
+    /**
+     * This function converts String XML to Document object
+     * @param in - XML String
+     * @return Document object
+     */
+    private static Document parseXmlFile(String in) {
+        try {
+            DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+            DocumentBuilder db = dbf.newDocumentBuilder();
+            InputSource is = new InputSource(new StringReader(in));
+            return db.parse(is);
+        } catch (ParserConfigurationException e) {
+            throw new RuntimeException(e);
+        } catch (SAXException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+    /**
+     * Takes an XML Document object and makes an XML String. Just a utility
+     * function.
+     *
+     * @param doc - The DOM document
+     * @return the XML String
+     */
+    public String makeXMLString(Document doc) {
+        String xmlString = "";
+        if (doc != null) {
+            try {
+                TransformerFactory transfac = TransformerFactory.newInstance();
+                Transformer trans = transfac.newTransformer();
+                trans.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "yes");
+                trans.setOutputProperty(OutputKeys.INDENT, "yes");
+                StringWriter sw = new StringWriter();
+                StreamResult result = new StreamResult(sw);
+                DOMSource source = new DOMSource(doc);
+                trans.transform(source, result);
+                xmlString = sw.toString();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return xmlString;
+    }
+}
