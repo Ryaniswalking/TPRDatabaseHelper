c3d060ae7a31b9492bf4d5ec20192f183c5665b0
ryanwalker <ryanwalker55@gmail.com>
2022-06-15 22:44:03 -0700

    move the view to its own class

--- src/main/java/TPRDatabaseHelper.java
+++ src/main/java/TPRDatabaseHelper.java
@@ -1,6 +1,7 @@
 import org.walker.tprDBHelper.controllers.DatabaseInfoController;
 import org.walker.tprDBHelper.models.CouchDBModel;
 import org.walker.tprDBHelper.views.DatabaseInfoForm;
+import org.walker.tprDBHelper.views.TPRDatabaseHelperView;
 import org.walker.tprDataConverter.controllers.FileInputController;
 import org.walker.tprDataConverter.models.FileInputModel;
 import org.walker.tprDataConverter.views.FileInputView;
@@ -18,46 +19,5 @@ public class TPRDatabaseHelper {
 //
 //        dbForm.setVisible(true);
         new TPRDatabaseHelperView();
-    }
-
-    static class TPRDatabaseHelperView extends JFrame {
-
-        TPRDatabaseHelperView(){
-            JPanel panel = new JPanel();
-
-            JButton dbHelperButton = new JButton("Database Helper");
-            JButton dataConverter = new JButton("Data Converter");
-
-            dbHelperButton.addActionListener(this::buttonPressed);
-            dataConverter.addActionListener(this::buttonPressed);
-
-            panel.add(dbHelperButton);
-            panel.add(dataConverter);
-
-            this.add(panel);
-
-            this.setSize(300, 200);
-            Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
-            this.setLocation(dim.width/2-this.getSize().width/2, dim.height/2-this.getSize().height/2);
-
-            this.setVisible(true);
-        }
-
-        private void buttonPressed(ActionEvent event) {
-            String actionEvent = event.getActionCommand();
-
-            if(actionEvent.equalsIgnoreCase("Database Helper")){
-                DatabaseInfoForm dbForm = new DatabaseInfoForm();
-                CouchDBModel dbModel = new CouchDBModel();
-
-                new DatabaseInfoController(dbForm, dbModel);
-
-                dbForm.setVisible(true);
-            }
-
-            if(actionEvent.equalsIgnoreCase("Data Converter")){
-                new FileInputController(new FileInputModel(), new FileInputView());
-            }
-        }
     }
 }
