1b2c76e9b7c85e5f10c03cd546e4c7fcadbb5f29
Ryaniswalking <97121935+ryanwalker55@users.noreply.github.com>
2022-06-21 13:49:49 -0700

    Add Logic for updated test case map
    updated test case map will mark the test case in red if it has been updated. Commit has logic to preserve the map when coming back from the test case view.

--- src/main/java/org/walker/tprDBHelper/controllers/ListTestCasesController.java
+++ src/main/java/org/walker/tprDBHelper/controllers/ListTestCasesController.java
@@ -15,6 +15,7 @@ import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.IOException;
+import java.util.HashMap;
 
 
 public class ListTestCasesController {
@@ -22,31 +23,55 @@ public class ListTestCasesController {
     private ListTestCasesModel ltcModel;
     private ListTestCasesView ltcView;
     private JSONObject couchDocumentsObj;
+    private HashMap<String, Boolean> updateTestCaseMap;
+
 
     public ListTestCasesController(ListTestCasesModel ltcModel, ListTestCasesView ltcView){
         this.ltcModel = ltcModel;
         this.ltcView = ltcView;
 
-        ltcView.displayAllTestCases(ltcModel.getTestCaseNamesList());
+        if(updateTestCaseMap == null || updateTestCaseMap.isEmpty()){
+            updateTestCaseMap = ltcModel.getNonUpdateTestCaseMap();
+        }
 
-        this.ltcView.addSelectedKeyNameListener(new SelectViewNameListener());
+        ltcView.displayAllTestCases(ltcModel.getTestCaseNamesList(), updateTestCaseMap);
+
+
+        this.ltcView.addSelectedKeyNameListener(new SelectTestCaseListener());
         this.ltcView.addSaveToCouchButtonListener(new SaveToCouchButtonLister());
         this.ltcView.addBackButtonListener(new BackButtonListener());
 
         this.ltcView.setVisible(true);
     }
 
+    //Construct coming forward from couch document list contain couch document obj
     public ListTestCasesController(ListTestCasesModel ltcModel, ListTestCasesView ltcView, JSONObject couchDocumentsObj){
         this(ltcModel,ltcView);
         this.couchDocumentsObj = couchDocumentsObj;
+
     }
 
-    public ListTestCasesController(ListTestCasesModel ltcModel, ListTestCasesView ltcView, JSONObject couchDocumentsObj, int scrollBarLocation){
-        this(ltcModel,ltcView, couchDocumentsObj);
+    //constructor when going back from TestCaseView, which holds the scroll bar location and the updated test case map
+    public ListTestCasesController(ListTestCasesModel ltcModel, ListTestCasesView ltcView, JSONObject couchDocumentsObj, int scrollBarLocation, HashMap<String, Boolean> updateTestCaseMap){
+        this.ltcModel = ltcModel;
+        this.ltcView = ltcView;
+        this.updateTestCaseMap = updateTestCaseMap;
+        this.couchDocumentsObj = couchDocumentsObj;
+
+
+        ltcView.displayAllTestCases(ltcModel.getTestCaseNamesList(), updateTestCaseMap);
+
+
+        this.ltcView.addSelectedKeyNameListener(new SelectTestCaseListener());
+        this.ltcView.addSaveToCouchButtonListener(new SaveToCouchButtonLister());
+        this.ltcView.addBackButtonListener(new BackButtonListener());
+
+        this.ltcView.setVisible(true);
+
         ltcView.setScrollBarLocation(scrollBarLocation);
     }
 
-    class SelectViewNameListener implements ActionListener {
+    class SelectTestCaseListener implements ActionListener {
 
         @Override
         public void actionPerformed(ActionEvent e) {
@@ -55,7 +80,13 @@ public class ListTestCasesController {
             int scrollBarLocation = ltcView.getScrollLocation();
 
             try {
-                new TestCaseController(new TestCaseModel(ltcModel.getTestCaseObj(keyName), ltcModel.getAllTestCasesObj(), keyName), new TestCaseView(), couchDocumentsObj, scrollBarLocation);
+                new TestCaseController(
+                        new TestCaseModel(ltcModel.getTestCaseObj(keyName), ltcModel.getAllTestCasesObj(), keyName),
+                        new TestCaseView(),
+                        couchDocumentsObj,
+                        scrollBarLocation,
+                        updateTestCaseMap);
+
             } catch (JSONException ex) {
                 ex.printStackTrace();
             }
@@ -67,39 +98,44 @@ public class ListTestCasesController {
 
         @Override
         public void actionPerformed(ActionEvent e) {
-            int result = JOptionPane.showConfirmDialog(ltcView,"Are you sure you want to save to Couch?", "Save Confirmation",
+            int result = JOptionPane.showConfirmDialog(ltcView, "Are you sure you want to save to Couch?", "Save Confirmation",
                     JOptionPane.YES_NO_OPTION,
                     JOptionPane.QUESTION_MESSAGE);
 
-            if(result == JOptionPane.YES_OPTION){
-                String couchKey = null;
-                try {
-                    couchKey = ltcModel.getCouchDocumentId();
-                } catch (JSONException ex) {
-                    ex.printStackTrace();
+            if (result == JOptionPane.NO_OPTION) {
+                return;
+            }
+
+            //Get Couch Key from model
+            String couchKey = null;
+            try {
+                couchKey = ltcModel.getCouchDocumentId();
+            } catch (JSONException ex) {
+                ex.printStackTrace();
+            }
+            JSONObject allTestCases = ltcModel.getAllTestCasesObj();
+
+            CouchDBModel couchdb = new CouchDBModel();
+
+            try {
+                //prettify all the test cases and save to couch using key
+                JSONObject prettyCouchDoc = Prettify.prettifyForCouch(allTestCases);
+                int response = couchdb.saveUpdatedTestCasesToCouch(couchKey, prettyCouchDoc);
+
+                if (response == 201) {
+                    JOptionPane.showMessageDialog(ltcView, "Couch Document Successfully Save");
+                    new CouchDocumentsController(new CouchDocumentsModel(couchDocumentsObj), new CouchDocumentsView());
+                    ltcView.setVisible(false);
+                } else {
+                    JOptionPane.showMessageDialog(ltcView, "Failed to Save Document to Couch");
                 }
-                JSONObject allTestCases = ltcModel.getAllTestCasesObj();
-
-                CouchDBModel couchdb = new CouchDBModel();
-
-                try {
-                    JSONObject prettyCouchDoc = Prettify.prettifyForCouch(allTestCases);
-                     int response = couchdb.saveUpdatedTestCasesToCouch(couchKey, prettyCouchDoc);
-
-                    if(response == 201){
-                        JOptionPane.showMessageDialog(ltcView, "Couch Document Successfully Save");
-                        new CouchDocumentsController(new CouchDocumentsModel(couchDocumentsObj), new CouchDocumentsView());
-                        ltcView.setVisible(false);
-                    }else{
-                        JOptionPane.showMessageDialog(ltcView,"Failed to Save Document to Couch");
-                    }
-                } catch (IOException ex) {
-                    ex.printStackTrace();
-                } catch (JSONException ex) {
-                    ex.printStackTrace();
-                }
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            } catch (JSONException ex) {
+                ex.printStackTrace();
             }
         }
+
     }
 
     class BackButtonListener implements ActionListener{
